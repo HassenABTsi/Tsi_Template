7e7e93976e5ee123b4be1bba0e9482c2f6173df2
Hassen Abbes <hassen.abbes@tsi.com.tn>
2021-08-06 12:49:01 +0100

    Ajout Update Product

--- src/Tsi.Template.Abstraction/Catalog/IProductService.cs
+++ src/Tsi.Template.Abstraction/Catalog/IProductService.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tsi.Template.Domain.Gesc.Catalog;
+using Tsi.Template.ViewModels.Catalog;
 
 namespace Tsi.Template.Abstraction.Catalog
 {
     public interface IProductService
     {
-        public Task<Product> CreateProductAsync(Product product);
-
+        public Task<Product> CreateProductAsync(Product product); 
         public Task DeleteProductAsync(int id);
-        Task<IEnumerable<Product>> GetAllAsync();
+        Task<IEnumerable<Product>> GetAllAsync(); 
+        Task<Product> GetProductByIdAsync(int id);
+
+        Task UpdateProductAsync(int id, ProductViewModel model);
     }
 }
--- src/Tsi.Template.Services/Catalog/ProductService.cs
+++ src/Tsi.Template.Services/Catalog/ProductService.cs
@@ -5,21 +5,22 @@ using Tsi.Template.Core.Attributes;
 using Tsi.Template.Domain.Gesc.Catalog;
 using Tsi.Template.Domain.Logging;
 using Tsi.Template.Infrastructure.Repository;
+using Tsi.Template.ViewModels.Catalog;
 
 namespace Tsi.Template.Services.Catalog
 {
     [Injectable(typeof(IProductService))]
     public class ProductService : IProductService
     {
-        private readonly IRepository<Product> _productRepo; 
+        private readonly IRepository<Product> _productRepo;
 
         public ProductService(IRepository<Product> productRepo)
         {
-            _productRepo = productRepo; 
+            _productRepo = productRepo;
         }
 
         public async Task<Product> CreateProductAsync(Product product)
-        { 
+        {
             return await _productRepo.AddAsync(product);
         }
 
@@ -32,5 +33,25 @@ namespace Tsi.Template.Services.Catalog
         {
             return await _productRepo.GetAllAsync();
         }
-    } 
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _productRepo.GetByIdAsync(id);
+        }
+
+        public async Task UpdateProductAsync(int id, ProductViewModel model)
+        {
+            var productToUpdate = await GetProductByIdAsync(id);
+
+            if(productToUpdate is null)
+            {
+                return;
+            }
+
+            productToUpdate.Libelle = model.Libelle;
+            productToUpdate.Price = model.Price;
+
+            await _productRepo.UpdateAsync(productToUpdate);
+        }
+    }
 }
